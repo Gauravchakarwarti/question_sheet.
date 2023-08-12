e7528fd2b1dfc470ed5e2dbdb5755727778cfc15
gaurav chakarwarti <136673555+Gauravchakarwarti@users.noreply.github.com>
2023-08-12 17:16:24 +0530

    Add files via upload

---
+++ worksheet.py
@@ -0,0 +1,82 @@
+#Question.  Find and read 20 interview questions for Data Types, Operators,
+ #Conditional Statements, Looping Statements, Functions.
+
+
+
+
+Data Types:
+    
+1.  What are the basic data types in Python?
+    Write a Python program to swap the values of two variables without using a temporary variable.
+
+2.  Explain the difference between value types and reference types.
+    Create a program that converts a given string to uppercase.
+
+3.  How do you convert a string to an integer in JavaScript?
+    Implement a function in Java that checks if a given year is a leap year.
+
+4.  What is type casting? Provide an example.
+     Write a C++ program to concatenate two strings without using the built-in concatenation function.
+
+5.  What is the significance of "null" in programming languages?
+     Develop a program in Python that calculates the area of a rectangle.
+
+Operators:
+
+1.    Create a program that determines if a given year is a leap year using the modulo operator.
+
+2.   What is operator precedence, and why is it important?
+     Implement a Python program to calculate the greatest common divisor (GCD) of two numbers.
+
+3.  How do you use the bitwise AND operator to check if a number is even or odd?
+    Create a C program to determine if a given character is a vowel or a consonant.
+
+4.   What is short-circuiting in logical operators?
+     Write a JavaScript function to find the largest element in an array.
+
+5.   Explain the difference between postfix and prefix increment operators.
+     Develop a C++ program that generates the Fibonacci sequence using a loop.
+
+Conditional Statements:
+
+1.   Describe the usage of the "switch" statement in C.
+     Write a Python program that determines whether a given number is prime.
+
+2.   How would you handle multiple conditions using "if-else if" statements?
+     Implement a Java program to calculate the sum of digits in a given number.
+
+3.   What is a nested ternary operator?
+     Create a C program that checks if a given year is a leap year using nested "if-else" statements.
+
+4.   Explain the concept of "short-circuit evaluation" in conditional statements.
+     Write a Python program to find the factorial of a number using a recursive function.
+
+5.   How do you avoid the "dangling else" problem in nested if-else statements?
+     Develop a Java program that determines if a given string is a palindrome.
+
+Looping Statements:
+    
+1.    Write a program that checks if a given number is a perfect square using a "for" loop.
+
+2.    Interview Question: How does the "break" statement work in loops? Provide an example.
+      Write a Python program to print the multiplication table of a given number using a "while" loop.
+
+3.   Explain the difference between a "while" loop and a "do-while" loop.
+     Create a Java program that calculates the sum of all even numbers up to a given limit using a loop.
+
+4.    Describe the use of the "continue" statement in loops.
+      Develop a C program to print the first "n" prime numbers.
+
+5.   How would you create an infinite loop intentionally? How would you break out of it?
+     Write a JavaScript program to check if a given number is a perfect square using a loop.
+
+Functions:
+    
+1.    What is the significance of function parameters and arguments?
+
+2.    Write a function that takes an array of numbers as input and returns the sum of all even numbers.
+3.
+4.    Implement a recursive function to calculate the nth term of the Fibonacci sequence.
+
+5.    How does scope affect variable accessibility within functions?
+    Describe the process of "calling" a function and "returning" from a function.
